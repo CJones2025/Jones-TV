cf568761d06a7fc28659f0998e6f512c4750c092
Cjones2025 <jonesyjones2032@gmail.com>
2025-07-24 19:55:51 -0500

    it is now showing all information, has been stylized

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,23 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p>Please select a show to watch.</p>;
+  }
+
+  return (
+    <>
+      <EpisodeList
+        episodes={show.episodes}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </>
+  );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,22 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ shows, setSelectedShow }) {
+  return (
+    <nav>
+      {shows.map((show) => (
+        <a
+          className="show"
+          key={show.name}
+          href="#"
+          onClick={(event) => {
+            event.preventDefault(); // prevent page jump
+            setSelectedShow(show);
+          }}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
